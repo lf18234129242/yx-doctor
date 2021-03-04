669291fdd90bfe1337a4caee90fffb922e5ef233
鲱鱼 <lf18234129242@163.com>
2021-03-04 09:49:35 +0800

    注册页面更新

--- src/view/kangduoduo/RegisterAll.vue
+++ src/view/kangduoduo/RegisterAll.vue
@@ -2,22 +2,33 @@
   <div class="patient-box">
     <h1>请输入您的信息</h1>
     <div class="cell-box">
-      <p class="user-name-label">姓名</p>
-      <input class="user-name" type="text" v-model="username" placeholder="请输入您的姓名" />
+      <p class="user-name-label">
+        姓名<span class="must">*</span>
+      </p>
+      <input class="user-name" type="text" v-model="filter.name" placeholder="请输入您的姓名" />
       <div class="flex-m gender-box" @click="handleChangeGender()">
-        <div :class="['gender-left flex-m-c', gender === 2 ? 'active-gender' : '']">
+        <div :class="['gender-left flex-m-c', filter.gender === 2 ? 'active-gender' : '']">
           <i class="iconfont iconxingbienv"></i>
           女士
         </div>
-        <div :class="['gender-right flex-m-c', gender === 1 ? 'active-gender' : '']">
+        <div :class="['gender-right flex-m-c', filter.gender === 1 ? 'active-gender' : '']">
           <i class="iconfont iconxingbienan"></i>
           男士
         </div>
       </div>
     </div>
     <div class="cell-box select-arrow" @click="selectAge">
-      <p>年龄</p>
-      <input @click="selectAge" disabled type="text" v-model="userage" placeholder="点击选择年龄" />
+      <p>年龄<span class="must">*</span></p>
+      <input @click="selectAge" disabled type="text" v-model="filter.age" placeholder="点击选择年龄" />
+    </div>
+    <div class="cell-box">
+      <p>手机<span class="must">*</span></p>
+      <input type="number" v-model="filter.phoneNumber" placeholder="请输入手机号" />
+    </div>
+    <div class="cell-box">
+      <p>验证码<span class="must">*</span></p>
+      <input class="reg_input" type="number" v-model="reg_num" placeholder="请输入验证码" />
+      <div class="get-reg-num" @click="getRegNum">{{timeout}}</div>
     </div>
     <div class="cell-box select-arrow" @click="showIll = true">
       <p>疾病</p>
@@ -27,14 +38,9 @@
       <p>病程</p>
       <input disabled type="text" v-model="illStepText" placeholder="点击选择病程" />
     </div>
-    <div class="cell-box">
-      <p>手机</p>
-      <input type="number" v-model="phone_num" placeholder="请输入手机号" />
-    </div>
-    <div class="cell-box">
-      <p>验证码</p>
-      <input class="reg_input" type="number" v-model="reg_num" placeholder="请输入验证码" />
-      <div class="get-reg-num" @click="getRegNum">{{timeout}}</div>
+    <div class="cell-box" @click="showIllStep = true">
+      <p>备注</p>
+      <input disabled type="text" v-model="filter.description" placeholder="请输入备注信息" />
     </div>
     <!-- 选择年龄弹出框 -->
     <van-popup v-model="show_age" round position="bottom">
@@ -83,63 +89,67 @@ export default {
   name: 'RegisterAll',
   data() {
     return {
-      illnesStepId: '',
+      filter: {
+        description: '',
+        province: '',
+        city: '',
+        area: '',
+        illnessId: '',
+        processId: '',
+        age: "",
+        phoneNumber: "",
+        name: "",
+        gender: 1,
+        token: "",
+        doctorId: ""
+      },
       illStepText: '',
-      illStepId: '',
       illStep: '',
       illText: '',
-      illnessId: '',
       illStepList: [],
       illList: [],
       showIllStep: false,
       showIll: false,
       disabledNext: false,
-      phone_num: "",
       reg_num: "",
       phoneCode: "",
-      username: "",
       usersex: "",
-      userage: "",
       timeout: "获取验证码",
       isGetRegNum: true,
-      gender: 1,
-      // show_sex: false,
       show_age: false,
       sex_columns: ["男", "女"],
       age_columns: [],
-      token: "",
       userId: "",
-      doctorId: "",
     };
   },
   mounted() {
     let href = window.location.href
-    this.token = getStrParam(href, "token");
+    this.filter.token = getStrParam(href, "token");
     this.userId = getStrParam(href, "userId");
-    this.doctorId = getStrParam(href, "doctorId");
-    sessionStorage.setItem("token", this.token);
+    this.filter.doctorId = getStrParam(href, "doctorId");
+    sessionStorage.setItem("token", this.filter.token);
     this.disabledNext = false
     this.getIllList()
   },
   methods: {
     onConfirmIllStep(value) {
-      this.illnesStepId = value.id
+      this.filter.processId = value.id
       this.illStepText = value.NAME
       this.showIllStep = false
     },
     onConfirmIll(value) {
-      this.illnessId = value.id
+      this.filter.illnessId = value.id
       this.illText = value.name
       this.showIll = false
       this.getIllStep()
     },
     handleChangeGender() {
-      this.gender = this.gender === 1 ? 2 : 1
+      this.filter.gender = this.filter.gender === 1 ? 2 : 1
     },
     getIllStep() {
       duoduo.newList({
-        token: this.token,
-        illnessId: this.illnessId
+        token: this.filter.token,
+        illnessId: this.filter.illnessId
       }).then(res => {
         if (res.data.code === 0) {
           this.illStepList = res.data.data
@@ -148,7 +158,7 @@ export default {
     },
     getIllList() {
       yinxing.getIllList({
-        token: this.token
+        token: this.filter.token
       }).then(res => {
         if (res.data.code === 0) {
           this.illList = res.data.data
@@ -157,10 +167,11 @@ export default {
     },
     // 下一步
     next() {
-      let username = this.username.trim().replace(XSSReg, '')
-      let phone_num = this.phone_num.trim().replace(XSSReg, '')
+      console.log(this.filter)
+      let name = this.filter.name.trim().replace(XSSReg, '')
+      let phoneNumber = this.filter.phoneNumber.trim().replace(XSSReg, '')
       let reg_num = this.reg_num.trim().replace(XSSReg, '')
-      if (username == "") {
+      if (name == "") {
         this.$toast("请输入您的姓名");
         return false;
       }
@@ -168,11 +179,11 @@ export default {
         this.$toast("请选择您的性别");
         return false;
       }
-      if (this.userage == "") {
+      if (this.filter.age == "") {
         this.$toast("请选择您的年龄");
         return false;
       }
-      if (!phone_num) {
+      if (!phoneNumber) {
         this.$toast("请输入您的手机号");
         return false;
       }
@@ -185,24 +196,16 @@ export default {
         return false;
       }
       this.disabledNext = true
-      yinxing.messageSave1({
-        doctorId: this.doctorId,
-        phoneNumber: phone_num,
-        age: this.userage,
-        gender: this.gender,
-        name: username,
-        token: this.token
-      }).then(res => {
+      yinxing.messageSave1(this.filter).then(res => {
         if (res.data.code === 0) {
-            this.$router.push({
-              path: "/SubmitTheIllness",
-              query: {
-                token: this.token,
-                userId: this.userId,
-                doctorId: this.doctorId
-              }
-            })
-          // }
+          this.$router.push({
+            path: "/SubmitTheIllness",
+            query: {
+              token: this.filter.token,
+              userId: this.userId,
+              doctorId: this.filter.doctorId
+            }
+          })
         }
       })
     },
@@ -210,11 +213,11 @@ export default {
       switch (event.target.innerHTML) {
         case "男":
           this.usersex = "男";
-          this.gender = 1;
+          this.filter.gender = 1;
           break;
         case "女":
           this.usersex = "女";
-          this.gender = 2;
+          this.filter.gender = 2;
           break;
         case "取消":
           break;
@@ -227,7 +230,7 @@ export default {
       this.show_age = true;
     },
     onConfirm_age(value) {
-      this.userage = value;
+      this.filter.age = value;
       this.show_age = false;
     },
     onCancel_age() {
@@ -237,11 +240,11 @@ export default {
     getRegNum() {
       let second = 60;
       let regPhoneNum = /^[1](([3][0-9])|([4][5-9])|([5][0-3,5-9])|([6][5,6])|([7][0-8])|([8][0-9])|([9][1,8,9]))[0-9]{8}$/;
-      if (!this.phone_num) {
+      if (!this.filter.phoneNumber) {
         this.$toast("请输入您的手机号");
         return false;
       }
-      if (!regPhoneNum.test(this.phone_num)) {
+      if (!regPhoneNum.test(this.filter.phoneNumber)) {
         this.$toast("请检查您的的手机号是否正确");
         return false;
       }
@@ -264,8 +267,8 @@ export default {
     getPhoneCode() {
       if (!this.isGetRegNum) return false
       yinxing.getPhoneCode({
-        phoneNumber: this.phone_num,
-        token: this.token
+        phoneNumber: this.filter.phoneNumber,
+        token: this.filter.token
       }).then(res => {
         this.$toast("验证码发送成功")
         this.phoneCode = res.data.verificationCode
@@ -314,13 +317,16 @@ export default {
   }
   .cell-box {
     width: 100%;
-    height: 2rem
+    height: 2.2rem
     display: flex;
     align-items center
     padding: 0 .8rem;
     box-sizing: border-box;
     display: flex;
     align-items: center;
+    .must{
+      color red
+    }
     .gender-box{
       width 5.1rem
       height 1.36rem
@@ -350,6 +356,15 @@ export default {
       line-height: 0.6rem;
       position: relative;
     }
+    p::after {
+      content: "";
+      position: absolute;
+      left: 2.5rem;
+      bottom: -0.8rem;
+      width: 10.1rem;
+      height: 0.02rem;
+      background: #d0d0d0;
+    }
     input {
       width: 8.1rem;
       height: 1rem;
@@ -358,22 +373,13 @@ export default {
       padding: 0;
       background: #fff;
     }
-    p::after {
-      content: "";
-      position: absolute;
-      left: 2.5rem;
-      bottom: -0.8rem;
-      width: 10.1rem;
-      height: 0.05rem;
-      background: #d0d0d0;
-    }
     .user-name-label::after {
       content: "";
       position: absolute;
       left: 2.5rem;
       bottom: -0.8rem;
       width: 5rem;
-      height: 0.05rem;
+      height: 0.02rem;
       background: #d0d0d0;
     }
   }
@@ -386,7 +392,7 @@ export default {
     height: 0.4rem;
     background: #acacac;
     position: absolute;
-    right: 1rem;
+    right: 1.6rem;
     top: .9rem;
     transform: rotate(45deg);
   }
@@ -396,7 +402,7 @@ export default {
     height: 0.4rem;
     background: #fff;
     position: absolute;
-    right: 1.04rem;
+    right: 1.64rem;
     top: .9rem;
     transform: rotate(45deg);
   }
